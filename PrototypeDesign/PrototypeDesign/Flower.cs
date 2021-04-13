2c8b5582677022407c5ecdda10b1b0d70ebcff4c
Sylvia Wan <sylvia.wan97@gmail.com>
2021-04-13 09:30:15 +0100

    Upload Prototype Pattern solution

---
+++ PrototypeDesign/PrototypeDesign/Flower.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDesign
+{
+
+    public abstract class Prototype
+    {
+        public abstract Prototype ShallowCopy();
+        public abstract Prototype DeepCopy();
+        public abstract void Debug();
+    }
+
+    public class FlowerInfo
+    {
+        public int id;
+
+        public FlowerInfo(int id)
+        {
+            this.id = id;
+        }
+    }
+    public class Flower : Prototype
+    {
+        public string flowerName;
+        public bool isNativeFlower;
+        public string[] flowerColors;
+        public FlowerInfo info;
+
+        public Flower(string name, bool nativeFlower, string[] color, FlowerInfo info)
+        {
+            this.flowerName = name;
+            this.isNativeFlower = nativeFlower;
+            this.flowerColors = color;
+            this.info = info;
+        }
+
+        public override Prototype ShallowCopy()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        public override Prototype DeepCopy()
+        {
+            Flower clonedFlower = (Flower)this.MemberwiseClone();
+            clonedFlower.info = new FlowerInfo(this.info.id);
+            return clonedFlower;
+        }
+
+        public override void Debug()
+        {
+            Console.WriteLine("- Flowers - ");
+            Console.WriteLine("Flower name: {0} \nNative: {1} ", this.flowerName, this.isNativeFlower);
+            Console.WriteLine("Flower ID: {0}", this.info.id);
+            Console.WriteLine("Flower colors: " + string.Join(",", flowerColors) + "\n");
+        }
+    } 
+}
---
+++ PrototypeDesign/PrototypeDesign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrototypeDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Native Island Flower -- ");
+
+            Flower originalFlower = new Flower("Lillies", true, new string[] { "Red", "White", "Yellow" }, new FlowerInfo(1));
+            originalFlower.Debug();
+
+            Console.WriteLine("-- Cloned Island Flower -- ");
+            Flower clonedFlower = (Flower)originalFlower.DeepCopy();
+            clonedFlower.Debug();
+
+            Console.WriteLine("Flower changes: ");
+            originalFlower.flowerName = "Tulips";
+            originalFlower.isNativeFlower = false;
+            originalFlower.info.id = 2;
+            originalFlower.Debug();
+            clonedFlower.Debug();
+        }
+    }
+}
